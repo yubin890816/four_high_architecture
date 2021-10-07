b96ecf0f1ae238fb01424ac0c2c98d32b88f51af
yubin03 <yubin03@xinye.com>
2021-10-07 23:19:25 +0800

    first commit

---
+++ src/main/java/com/yubin/architecture/four_high_architecture/FourHighArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.yubin.architecture.four_high_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourHighArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourHighArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yubin/architecture/four_high_architecture/FourHighArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yubin.architecture.four_high_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourHighArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
